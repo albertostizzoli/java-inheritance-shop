58a9aef3025b261c28313b5904e693220e663fe4
albertostizzoli <albertostizzoli@yahoo.it>
2024-04-02 15:27:36 +0200

    create class HeadPhones

---
+++ src/org/experis/inheritance/HeadPhones.java
@@ -0,0 +1,31 @@
+package org.experis.inheritance;
+
+public class HeadPhones extends Product{
+    //Attributi
+    private String colour;
+    private boolean wireless;
+
+    //Costruttore
+    public HeadPhones(int code, String name, String brand, double price, double iva, String colour, boolean wireless){
+        super(code, name, brand, price, iva);
+        this.colour = colour;
+        this.wireless = wireless;
+    }
+
+    // Metodi Getter e Setter specifici per HeadPhones
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public boolean isWireless() {
+        return wireless;
+    }
+
+    public void setWireless(boolean wireless) {
+        this.wireless = wireless;
+    }
+}
