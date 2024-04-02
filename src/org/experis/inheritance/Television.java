e568b47794e77a263b66b1d4a3c90ab0b2a3f300
albertostizzoli <albertostizzoli@yahoo.it>
2024-04-02 15:14:51 +0200

    create class Television

---
+++ src/org/experis/inheritance/Television.java
@@ -0,0 +1,33 @@
+package org.experis.inheritance;
+
+public class Television extends Product{
+    //Attributi
+    private String dimensions;
+    private boolean smart;
+
+    //Costruttore
+    public Television(int code, String name, String brand, double price, double iva, String dimensions, boolean smart){
+        super(code, name, brand, price, iva);
+        this.dimensions = dimensions;
+        this.smart = smart;
+    }
+
+    // Metodi Getter e Setter specifici per Television
+    public String getDimensions() {
+        return dimensions;
+    }
+
+    public void setDimensions(String dimensions) {
+        this.dimensions = dimensions;
+    }
+
+    public boolean isSmart() {
+        return smart;
+    }
+
+    public void setSmart(boolean smart) {
+        this.smart = smart;
+    }
+}
+
+
